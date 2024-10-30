72ba943380244113cc23ec19d7999fc4fd77f604
David L. Almeida <contato@davidcreator.com>
2024-10-29 23:23:29 -0300

    Update Controller Multi Components

---
+++ 02 - Scripts/01 - components/03 - Multi Components/js/script.js
@@ -0,0 +1,47 @@
+var myGamePiece; // Variável para armazenar a peça do jogo
+var myGamePiece2; // Variável para armazenar a segunda peça
+var myGamePiece3; // Variável para armazenar a terceira peça
+var myGamePiece4; // Variável para armazenar a quarta peça
+
+function startGame() {
+    myGameArea.start(); // Inicializa a área do jogo
+    myGamePiece = new component(30, 30, "red", 10, 120); // Cria a primeira peça do jogo com tamanho e posição especificados
+    myGamePiece2 = new component(50, 50, "blue", 300, 150); // Cria a segunda peça do jogo com tamanho e posição especificados
+    myGamePiece3 = new component(20, 20, "green", 50, 50); // Cria a terceira peça do jogo com tamanho e posição especificados
+    myGamePiece4 = new component(40, 40, "yellow", 200, 100); // Cria a quarta peça do jogo com tamanho e posição especificados
+}
+
+var myGameArea = {
+    canvas: document.createElement("canvas"), // Cria um novo elemento canvas
+    start: function() {
+        this.canvas.width = 480; // Define a largura do canvas
+        this.canvas.height = 270; // Define a altura do canvas
+        this.context = this.canvas.getContext("2d"); // Obtém o contexto 2D do canvas
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]); // Insere o canvas no corpo do documento
+        this.interval = setInterval(updateGameArea, 20); // Define um intervalo para atualizar a área do jogo
+    },
+    clear: function() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height); // Limpa a área do canvas
+    }
+};
+
+function component(width, height, color, x, y) {
+    this.width = width; // Define a largura do componente
+    this.height = height; // Define a altura do componente
+    this.x = x; // Define a posição x do componente
+    this.y = y; // Define a posição y do componente
+    this.update = function() {
+        ctx = myGameArea.context; // Obtém o contexto do canvas
+        ctx.fillStyle = color; // Define a cor de preenchimento
+        ctx.fillRect(this.x, this.y, this.width, this.height); // Desenha o componente no canvas
+    }
+}
+
+// Função para atualizar a área do jogo a cada frame
+function updateGameArea() {
+    myGameArea.clear(); // Limpa a área do canvas
+    myGamePiece.update(); // Atualiza e desenha a primeira peça do jogo na nova posição
+    myGamePiece2.update(); // Atualiza e desenha a segunda peça do jogo na nova posição
+    myGamePiece3.update(); // Atualiza e desenha a terceira peça do jogo na nova posição
+    myGamePiece4.update(); // Atualiza e desenha a quarta peça do jogo na nova posição
+}
